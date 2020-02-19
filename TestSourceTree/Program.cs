b478f06657d370a2c41653146fe4979d8ceb7d79
Phat Tai <dangvophattai99@gmail.com>
2020-02-19 18:03:18 +0700

    Hoang hello

--- TestSourceTree/Program.cs
+++ TestSourceTree/Program.cs
@@ -6,6 +6,7 @@ namespace TestSourceTree
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hoang Hello!");
             Console.WriteLine("Lisa Hello");
             Console.WriteLine("Tai Hello!");
             Console.ReadKey();
