4271c480ab1094ca568d72335bd8981faf2c0f02
Phat Tai <dangvophattai99@gmail.com>
2020-02-19 15:49:27 +0700

    Second Commit

--- TestSourceTree/Program.cs
+++ TestSourceTree/Program.cs
@@ -7,6 +7,7 @@ namespace TestSourceTree
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Tai Hello!");
             Console.ReadKey();
         }
     }
